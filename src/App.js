433837ff12276e8a6f15450c7f1b0002d658e11b
alejandro.cantu1018@gmail.com <Alejandro Cantu>
2023-03-14 17:39:05 -0500

    Added MUI, and auth. Clean up hook vars plzzzzz.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,24 @@
 import { useEffect, useState } from "react";
+import { initializeApp } from "firebase/app";
+import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword } from "firebase/auth";
+import {
+  TextField,
+  Button,
+  Select,
+  MenuItem,
+  InputLabel,
+  FormControl
+} from "@mui/material";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBXJWfoOa4zXC8i-xJyADFYUC3C9JOq6bQ",
+
+  databaseURL: "https://console.firebase.google.com/u/0/project/hci-app-2e9f2/database/hci-app-2e9f2-default-rtdb/data/~2F",
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+// const db = getFirestore(app);
 
 export default function App() {
 
@@ -8,6 +28,13 @@ export default function App() {
   const [city, setCity] = useState("Austin");
   const [temps, setTemps] = useState(null);
   const [times, setTimes] = useState(null);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [cityList, setCityList] = useState(["Houston", "Dallas", "Austin"]);
+  const [currentCity, setCurrentCity] = useState(cityList[0]);
+  const [cityToOperate, setCityToOperate] = useState("");
+
+
 
   useEffect(() => {
     gatherWeatherData(city, 0);
@@ -81,53 +108,169 @@ export default function App() {
     }
   }
 
+  async function signUpUser() {
+    setUsername("");
+    setPassword("");
+
+    createUserWithEmailAndPassword(auth, username, password)
+      .then((userCredential) => {
+        const user = userCredential.user;
+        console.log(user);
+      })
+      .catch((error) => {
+        console.log("Error\n Code:" + error.code + " \nMessage:" + error.message);
+      });
+  }
+
+  async function loginUser() {
+    setUsername("");
+    setPassword("");
+
+    signInWithEmailAndPassword(auth, username, password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        // ...
+        console.log(user);
+        // Load user data for that username
+        // setCityList
+      })
+      .catch((error) => {
+        console.log("Error\n Code:" + error.code + " \nMessage:" + error.message);
+      });
+  }
+
+  function pageDisplay(loggedIn) {
+    if (loggedIn) {
+      return (
+        <div className="container">
+          <div className="row">
+            {/* Saved Cities */}
+            <div className="col-sm-auto">
+              <FormControl fullWidth>
+                <InputLabel id="select-label">Saved Cities</InputLabel>
+                <Select
+                  labelId="select-label"
+                  id="simple-select"
+                  value={currentCity}
+                  label="Saved Cities"
+                  onChange={setCurrentCity}
+                >
+                  {cityList &&
+                    cityList.map(city =>
+                      <MenuItem value={city}>{city}</MenuItem>
+                    )}
+
+                </Select>
+              </FormControl>
+            </div>
+            {/* City Search or Add */}
+            <div className="col-sm-auto"> {/* MOVE THIS THING TO THE RIGHT */}
+              <div className="row">
+              <TextField
+                id="city-search"
+                label="Type A City"
+                value={cityToOperate}
+                onChange={() => setCityToOperate}>
+              </TextField>
+              </div>
+              <div className="row">
+              <div className="col-sm-auto">
+              <Button variant="contained" >
+          Add to Saved
+        </Button>
+              </div>
+              <div className="col-sm-auto">
+              <Button variant="contained" >
+          Search 
+        </Button>
+              </div>
+              </div>
+              
+            </div>
+          </div>
+
+          
+
+          
+
+
+{/* 
+          <div className="row ">
+            <div className="col-sm city ">
+              <button className="city-button" onClick={() => gatherWeatherData("houston", 0)}>Houston</button>
+            </div>
+            <div className="col-sm-auto city">
+              <button className="city-button" onClick={() => gatherWeatherData("dallas", 0)}>Dallas</button>
+            </div>
+            <div className="col-sm city ">
+              <button className="city-button" onClick={() => gatherWeatherData("austin", 0)}>Austin</button>
+            </div>
+          </div> */}
+
+          {/* Search Bar */}
+          <div className="row search-row">
+            <div className="col-sm-auto">
+              <input id="lat-input" type="search" placeholder="Latitude" onChange={(e) => setLat(e.target.value)} />
+            </div>
+            <div className="col-sm-auto">
+              <input id="lon-input" type="search" placeholder="Longitude" onChange={(e) => setLon(e.target.value)} />
+            </div>
+            <div className="col-sm-auto search">
+              <button id="search-button" className="search-button" onClick={() => gatherWeatherData(null, 1)}>Search</button>
+            </div>
+          </div>
+
+
+          {/* Weather Data */}
+          <div className='current-city'>
+            Today's Weather in {city}
+          </div>
+          {
+            data && data.map(hour =>
+              <div className='row hourly'>
+                <div className='col-sm-auto time'>
+                  {hour[0]}
+                </div>
+                <div className='col-sm-auto'>
+                  {hour[1]}
+                </div>
+              </div>
+            )
+          };
+        </div>
+      );
+    } else { /* USER LOGIN SCREEN BELOW */ 
+      return (
+        <div className="container">
+          <div className="row">
+            <input id="username" type="email" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} />
+          </div>
+          <div className="row">
+            <input id="password" type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+          </div>
+          <div className="row login-button">
+            <button id="login" onClick={() => loginUser()}>Login</button>
+          </div>
+          <div className="row login-button">
+            <button id="createNewUser" onClick={() => signUpUser()}>Create New User and Login</button>
+          </div>
+        </div>
+      );
+
+    }
+  }
+
   return (
     <div>
     {/* Title */}
     <h1 className="title">Texas Sun</h1>
-    
+    {/* {pageDisplay(0)} */}
+
+
     {/* City Buttons */}
-    <div className="row ">  
-      <div className="col-sm city ">
-        <button className="city-button" onClick={() =>gatherWeatherData("houston", 0)}>Houston</button>
-      </div>
-      <div className="col-sm-auto city">
-        <button className="city-button" onClick={() =>gatherWeatherData("dallas", 0)}>Dallas</button>
-      </div>
-      <div className="col-sm city ">
-        <button className="city-button" onClick={() =>gatherWeatherData("austin", 0)}>Austin</button>
-      </div>
-    </div>
-
-    {/* Search Bar */}
-    <div className="row search-row">
-      <div className="col-sm-auto">
-        <input id="lat-input" type="search" placeholder="Latitude" onChange={(e) => setLat(e.target.value)}/>
-      </div>
-      <div className="col-sm-auto">
-        <input id="lon-input" type="search" placeholder="Longitude" onChange={(e) => setLon(e.target.value)}/>
-      </div>
-      <div className="col-sm-auto search">
-        <button id="search-button" className="search-button" onClick={() => gatherWeatherData(null, 1)}>Search</button>
-      </div>
-    </div>
-
+    {pageDisplay(1)}
     
-    {/* Weather Data */}
-    <div className='current-city'>
-    
-    Today's Weather in {city}
-    </div>
-      {data && data.map(hour =>
-      <div className='row hourly'>
-        <div className='col-sm-auto time'>
-          {hour[0]}
-        </div>
-        <div className='col-sm-auto'>
-          {hour[1]}
-        </div>
-      </div>
-      )};
     </div>
     
   )
