c464c90b881c97195eea8e56c10f430868814e73
alejandro.cantu1018@gmail.com <Alejandro Cantu>
2023-03-15 13:52:51 -0500

    Added city lookup

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ export default function App() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [cityList, setCityList] = useState(["Houston", "Dallas", "Austin"]);
-  const [currentCity, setCurrentCity] = useState(cityList[0]);
+  const [currentCity, setCurrentCity] = useState("Houston");
   const [cityToOperate, setCityToOperate] = useState("");
 
 
@@ -40,63 +40,41 @@ export default function App() {
     gatherWeatherData(city, 0);
   }, []);
 
-  async function gatherWeatherData(city, search) {
+  async function gatherWeatherData() {
     const forecast_url = "https://api.open-meteo.com/v1/forecast?";
-    const url_suffix = "&hourly=temperature_2m&temperature_unit=fahrenheit"
-    const htx_coord = ["29.76", "-95.36"];
-    const atx_coord = ["30.27", "-97.74"];
-    const dtx_coord = ["32.78", "-96.81"];
+    const geolocator_url = "https://geocoding-api.open-meteo.com/v1/search";
+    const geolocator_suffix = "?name="
+    const url_suffix = "&hourly=temperature_2m&temperature_unit=fahrenheit";
     const lat_s = "latitude=";
     const lon_s = "&longitude="
-    let coord;
-  
-    if (search) {
-      // search
-      const regex = new RegExp('^[-]?([0-9]+[.]?[0-9]*)');
-      if (!(regex.test(lat) && regex.test(lon))) {
-        alert("Incorrect coordinate format in search bar. Please correct and try again. Defaulting to Austin weather reports.")
-        coord = atx_coord;
-        city = "Austin";
-      } else {
-        coord = [lat, lon];
-        city = lat + ", " + lon
-  
-      }
-      
-    } else {
-      switch (city) {
-        case "houston":
-          coord = htx_coord;
-          city = "Houston";
-          break;
-        case "dallas":
-          coord = dtx_coord;
-          city = "Dallas";
-          break;
-        default:
-        case "austin":
-            coord = atx_coord;
-            city = "Austin";
-            break;
-      }
-    }
-    setLat(coord[0]);
-    setLon(coord[1]);
-    setCity(city);
-
-  
-    let fetch_url = forecast_url + lat_s + coord[0] + lon_s + coord[1] + url_suffix;
   
     try {
-      const response = await fetch(fetch_url);
-      const json = await response.json();
+      setCityToOperate("");
+      // 
+      let url = geolocator_url + geolocator_suffix + cityToOperate;
+      let response = await fetch(url);
+      let json = await response.json();
+      let city = json["results"][0]
+      console.log(city);
+      let searchLat = city["latitude"];
+      let searchLon = city["longitude"];
+      setLat(searchLat);
+      setLon(searchLon);
+      console.assert(cityToOperate != ""); // just to check if that's how React Hooks work
+      // Capitalize City Name
+      setCity(city["name"]);
+
+      let fetch_url = forecast_url + lat_s + searchLat + lon_s + searchLon + url_suffix;
+
+      response = await fetch(fetch_url);
+      json = await response.json();
       // update Weather Data
 
       let temp = json.hourly.temperature_2m.slice(0, 10);
-      temp = temp.map((x) => x.toString().substring(0,2) + "\u00B0F")
+      temp = temp.map((x) => x.toString().substring(0, 2) + "\u00B0F")
 
-      let time = json.hourly.time.slice(0,10);
-      time = time.map((x) => x.substring(11).substring(1,2) + "PM");
+      let time = json.hourly.time.slice(0, 10);
+      time = time.map((x) => x.substring(11).substring(1, 2) + "PM");
       time[0] = '12PM'
 
       let daterr = time.map((x, i) => [x, temp[i]]);
@@ -106,6 +84,14 @@ export default function App() {
     } catch (err) {
       console.log(err);
     }
+  }
+
+  const handleCurrentCityChange = (event) => {
+    setCurrentCity(event.target.value);
+  }
+
+  const handleCityBarChange = (event) => {
+    setCityToOperate(event.target.value);
   }
 
   async function signUpUser() {
@@ -140,11 +126,20 @@ export default function App() {
       });
   }
 
+  async function addToSaved() {
+    // setCityToOperate("");
+    console.log(cityToOperate);
+  }
+
+  async function searchCity() {
+    console.log(cityToOperate);
+  }
+
   function pageDisplay(loggedIn) {
     if (loggedIn) {
       return (
         <div className="container">
-          <div className="row">
+          <div className="row search-row">
             {/* Saved Cities */}
             <div className="col-sm-auto">
               <FormControl fullWidth>
@@ -154,7 +149,7 @@ export default function App() {
                   id="simple-select"
                   value={currentCity}
                   label="Saved Cities"
-                  onChange={setCurrentCity}
+                  onChange={handleCurrentCityChange}
                 >
                   {cityList &&
                     cityList.map(city =>
@@ -167,60 +162,28 @@ export default function App() {
             {/* City Search or Add */}
             <div className="col-sm-auto"> {/* MOVE THIS THING TO THE RIGHT */}
               <div className="row">
-              <TextField
-                id="city-search"
-                label="Type A City"
-                value={cityToOperate}
-                onChange={() => setCityToOperate}>
-              </TextField>
+                <TextField
+                  id="city-search"
+                  label="Type A City"
+                  value={cityToOperate}
+                  onChange={handleCityBarChange}>
+                </TextField>
               </div>
               <div className="row">
-              <div className="col-sm-auto">
-              <Button variant="contained" >
-          Add to Saved
-        </Button>
+                <div className="col-sm-auto">
+                  <Button variant="contained" onClick={() => addToSaved()}>
+                    Add to Saved
+                  </Button>
+                </div>
+                <div className="col-sm-auto">
+                  <Button variant="contained" onClick={() => gatherWeatherData()}>
+                    Search
+                  </Button>
+                </div>
               </div>
-              <div className="col-sm-auto">
-              <Button variant="contained" >
-          Search 
-        </Button>
-              </div>
-              </div>
-              
+
             </div>
           </div>
-
-          
-
-          
-
-
-{/* 
-          <div className="row ">
-            <div className="col-sm city ">
-              <button className="city-button" onClick={() => gatherWeatherData("houston", 0)}>Houston</button>
-            </div>
-            <div className="col-sm-auto city">
-              <button className="city-button" onClick={() => gatherWeatherData("dallas", 0)}>Dallas</button>
-            </div>
-            <div className="col-sm city ">
-              <button className="city-button" onClick={() => gatherWeatherData("austin", 0)}>Austin</button>
-            </div>
-          </div> */}
-
-          {/* Search Bar */}
-          <div className="row search-row">
-            <div className="col-sm-auto">
-              <input id="lat-input" type="search" placeholder="Latitude" onChange={(e) => setLat(e.target.value)} />
-            </div>
-            <div className="col-sm-auto">
-              <input id="lon-input" type="search" placeholder="Longitude" onChange={(e) => setLon(e.target.value)} />
-            </div>
-            <div className="col-sm-auto search">
-              <button id="search-button" className="search-button" onClick={() => gatherWeatherData(null, 1)}>Search</button>
-            </div>
-          </div>
-
 
           {/* Weather Data */}
           <div className='current-city'>
@@ -243,18 +206,24 @@ export default function App() {
     } else { /* USER LOGIN SCREEN BELOW */ 
       return (
         <div className="container">
-          <div className="row">
-            <input id="username" type="email" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} />
+          <div className="row search-row">
+            <div className="col-sm-auto">
+              <TextField id="username" label="Username" value={username} onChange={(e) => setUsername(e.target.value)} />
+            </div>
+            <div className="col-sm-auto">
+              <TextField id="password" label="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+            </div>
           </div>
-          <div className="row">
-            <input id="password" type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+          
+          <div className="row search-row">
+            <div className="col-sm-auto">
+              <Button variant="contained" id="login" onClick={() => loginUser()}>Login</Button>
+            </div>
+            <div className="col-sm-auto">
+              <Button variant="contained" id="createNewUser" onClick={() => signUpUser()}>Create New User and Login</Button>
+            </div>
           </div>
-          <div className="row login-button">
-            <button id="login" onClick={() => loginUser()}>Login</button>
-          </div>
-          <div className="row login-button">
-            <button id="createNewUser" onClick={() => signUpUser()}>Create New User and Login</button>
-          </div>
+          
         </div>
       );
 
